9f1d20b5b4e9cbf95863c3455b5608150504112c
HZeligman <103866815+HZeligman@users.noreply.github.com>
2022-12-08 20:32:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ mongoose.connect(process.env.CONNECTION_URI, {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://localhost:64048', 'https://mycinemamovieapp.herokuapp.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://mycinemamovieapp.herokuapp.com'];
 
 const cors = require('cors');
 app.use(cors({
